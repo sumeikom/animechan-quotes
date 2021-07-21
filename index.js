748c5429f479dcbb619f0e8cbad580269a82efe3
Sumei Kom <sumeikom@gmail.com>
2021-07-21 10:03:02 -0400

    index.js FIXED

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const URL = 'http://localhost:3000/spells';
 
+fetch(URL)
+.then(response => response.json())
+.then(data => {
+  console.log(data);
+  const ul = document.querySelector('#spell-information');
+  
+  data.forEach(spells => {
+    const interspells = `${spells.index} ${spells.name} ${spells.url}`;
+    const li = document.createElement('li')
+    li.textContent = interspells
+    ul.append(li);
+  })
+})
