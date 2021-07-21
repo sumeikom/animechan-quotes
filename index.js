c8b2cdd0ca01b2d0ee3904b37b69b08fa1efa1eb
Sumei Kom <sumeikom@gmail.com>
2021-07-21 12:10:57 -0400

    added anime save bttn

--- index.js
+++ index.js
@@ -3,3 +3,19 @@ const reloadAnimeBttn = document.getElementById('button');
 const upvoteCount = document.getElementById('upvotes');
 const animeTitle = document.getElementById("meme-title");
 const saveAnimeBttn = document.getElementById("save-button")
+
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("HTML has Loaded!");
+
+    reloadAnimeBttn.addEventListener('click', getAnimeQuote)
+
+    saveAnimeBttn.addEventListener('click', saveAnimeQuote)
+
+    upvoteCount.addEventListener('mouseover', (e) => {
+        e.target.style.color = "pink"
+    });
+    
+    upvoteCount.addEventListener('mouseout', (e) => {
+        e.target.style.color = "black"
+    });
+});
