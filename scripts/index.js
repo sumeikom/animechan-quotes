ca314f84b39e6ab8e221b627ca5f372559fb3694
Sumei Kom <sumeikom@gmail.com>
2021-07-21 11:51:47 -0400

    removed services

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,47 @@
-const URL = 'http://localhost:3000/spells';
 
-fetch(URL)
-.then(response => response.json())
-.then(data => {
-  console.log(data);
-  const ul = document.querySelector('#spell-information');
+// api url
+const api_url = 
+      "https://www.dnd5eapi.co/api/spells";
   
-  data.forEach(spells => {
-    const interspells = `${spells.index} ${spells.name} ${spells.url}`;
-    const li = document.createElement('li')
-    li.textContent = interspells
-    ul.append(li);
-  })
-})
+// Defining async function
+async function getapi(url) {
+    
+    // Storing response
+    const response = await fetch(url);
+    
+    // Storing data in form of JSON
+    var data = await response.json();
+    console.log(data);
+    if (response) {
+        hideloader();
+    }
+    show(data);
+}
+// Calling that async function
+getapi(api_url);
+  
+// Function to hide the loader
+function hideloader() {
+    document.getElementById('loading').style.display = 'none';
+}
+// Function to define innerHTML for HTML table
+function show(data) {
+    let tab = 
+        `<tr>
+          <th>Index</th>
+          <th>Name</th>
+          <th>Url</th>
+         </tr>`;
+    
+    // Loop to access all rows 
+    for (let r of data.list) {
+        tab += `<tr> 
+    <td>${r.index} </td>
+    <td>${r.name}</td>
+    <td>${r.url}</td> 
+          
+</tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("spells").innerHTML = tab;
+}
